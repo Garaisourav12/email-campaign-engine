59d502f548eb89e1d42e28dfb82ed9cd9d68f3c7
Garaisourav12 <garaisourav12@gmail.com>
2025-08-03 09:37:35 +0530

    getEmailtemplates api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
 
 const validateToken = require("../middlewares/authMiddleware");
+const emailTemplates = require("../utils/emailTemplates");
 const authRouter = require("./authRoutes");
 const campaignRouter = require("./campaignRoutes");
 
 router.use("/auth", authRouter);
 router.use("/campaigns", validateToken, campaignRouter);
+router.get("/getEmailTemplates", (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .json({
+        success: true,
+        data: emailTemplates,
+        message: "Email templates fetched successfully",
+      });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
